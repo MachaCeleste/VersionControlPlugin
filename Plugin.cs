773869038fa24a9e8380a360678bf51355ffa86a
Macha Celeste <shetah1@yahoo.com>
2025-05-01 12:55:08 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace VersionControlPlugin;
+
+[BepInPlugin("com.machaceleste.versioncontrolplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Grey Hack.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> initialVersions;
+
+
+    private void Awake()
+    {
+        initialVersions = Config.Bind("Main", "Initial Library Versions", 0, new ConfigDescription("Sets the initial number of library versions for the system to generate.", new AcceptableValueRange<int>(0, 10)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"VersionControlPlugin is loaded!");
+        var harmony = new Harmony("com.machaceleste.versioncontrolplugin");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/VersionControlPatch.cs
@@ -0,0 +1,29 @@
+﻿using VersionControlPlugin;
+using HarmonyLib;
+
+[HarmonyPatch]
+public class VersionControlPatch
+{
+    [HarmonyPatch(typeof(VersionsControl), "Configure")]
+    class ConfigurePatch
+    {
+        static void Postfix(VersionsControl __instance)
+        {
+            int n = Plugin.initialVersions.Value;
+            var libs = __instance.GetLastLibs();
+            if (n < 1 || libs["metaxploit.so"].GetStrVersion() != "1.0.0") return;
+            var rnd = new System.Random();
+            for (var i = 0; i < n; i++)
+            {
+                foreach (var lib in libs)
+                {
+                    var mem = lib.Value.GetRandomMem(rnd);
+                    var vulns = mem.vulnerabs;
+                    vulns[rnd.Next(0, vulns.Count)].helperHackResult.vecesUsado = VersionsControl.MAX_USED_EXPLOIT;
+                }
+                var method = AccessTools.Method(typeof(VersionsControl), "ThreadCheckVersions");
+                method.Invoke(__instance, new object[] { libs["metaxploit.so"] });
+            }
+        }
+    }
+}
